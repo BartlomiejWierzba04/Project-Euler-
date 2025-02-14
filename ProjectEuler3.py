0d20e058fe562c521606a6cf4a9bf8e4ffc811dd
Bartlomiej <bartlomiejwierzba04@gmail.com>
2025-02-14 06:46:02 +0000

    Project Euler 3 Added

---
+++ ProjectEuler3.py
@@ -0,0 +1,18 @@
+## Project Euler Problem 3 
+## What is the largest prime factor of the number 600851475143
+
+
+num = 600851475143
+print(num)
+
+
+#how do we iterate the primes?
+# don't need to, just need to add
+
+primes = [2,3,5,7,11,13,17]
+num = 2
+
+def generatePrimes():
+    for x in range(0,number):
+    
+    
