c9294d29192f7d721ee4be62cd15b29777c50789
Bartlomiej <bartlomiejwierzba04@gmail.com>
2025-02-20 23:32:43 +0000

    Added ProjectEuler9

---
+++ ProjectEuler9.py
@@ -0,0 +1,18 @@
+#There exists exactly one Pythagorean triplet for which a+b+c = 1000
+#Find the product abc 
+import math 
+
+
+#a^2+b^2=c^2 makes a pythagorean triplet a b c 
+def generateTriplets():
+    TripletArr = []
+    for x in range(1,1000): 
+        for y in range(1,1000): 
+            result = int(math.sqrt(x**2+y**2))
+            if x**2+y**2 == (int(result)**2) and(x+y+result == 1000):
+                print(result*x*y)
+
+
+
+
+generateTriplets()
